0cb8154b0bcc542590e21431a05112f4521062b3
Lenart <never.saymeme@gmail.com>
2020-01-10 15:59:57 +0100

    Work on new new renderer

--- String_Game/String_Game.cpp
+++ String_Game/String_Game.cpp
@@ -12,9 +12,9 @@ public:
 	int y;
 };
 
-double get_map(int seed, int, int x, int y);
+string get_map(int, int, int, int);
 
-void Load_Map(int, int, player);
+void Load_Map(string, int, player);
 
 /*int main() {
 	int a = 1;
@@ -35,11 +35,12 @@ int main()
 	cin >> seed;
 	cout << "Vnesi sensitivity: ";
 	cin >> sensitivity;
-	bool Game = true, Input_Buffer = false; int izbira, x, yr;
+	string map = get_map(seed, sensitivity, 100, 100);
+	bool Game = true, Input_Buffer = false;
 	player Pl;
 	Pl.x = 0;
 	Pl.y = 0;
-	Load_Map(seed, sensitivity, Pl);
+	Load_Map(map, 5000, Pl);
 	while (Game == true) {
 		if (kbhit() && Input_Buffer == false) {
 			Input_Buffer = true;
@@ -53,7 +54,7 @@ int main()
 			else if (input == 'a')
 				Pl.x -= 1;
 			system("CLS");
-			Load_Map(seed, sensitivity, Pl);
+			Load_Map(map, 2500, Pl);
 			rlutil::setColor(7);
 			cout << endl;
 			rlutil::setColor(7);
@@ -62,76 +63,68 @@ int main()
 			Input_Buffer = false;
 
 		}
-		/*
-		cout << "Izberi med opcijami: " << endl;
-		cout << "1.) Premik" << endl;
-		cout << "9.) Konec" << endl;
-		cout << "Vpisi izbiro: ";
-		cin >> izbira;
-		switch (izbira) {
-		case 1:
-			cout << "Vpisi premik po x osi: ";
-			cin >> x;															
-			cout << "Vpisi premik po y osi: ";
-			cin >> y;
-			Pl.x += x;
-			Pl.y += y;
-			break;
-		case 9:
-			Game = false;
-			break;
-		}
-		*/
 	}
 	return 0;
 }
 
-double get_map(int seed, int sensitivity, int x, int y) {
+string get_map(int seed, int sensitivity, int velikost, int visina) {
 	siv::PerlinNoise pn(seed);
-	double x1 = (double)x / ((double)sensitivity);
-	double y1 = (double)y / ((double)sensitivity);
+	string map;
+	for (int y = 0; y < visina; y++) {
+		for (int x = 0; x < velikost; x++) {
+			double x1 = (double)x / ((double)sensitivity);
+			double y1 = (double)y / ((double)sensitivity);
 
-	double n = pn.noise(10 * x1, 10 * y1, 0.8);
-	return n;
+			double n = pn.noise(10 * x1, 10 * y1, 0.8);
+			if (n < 0.2)
+				map += (char)176;
+			else if (n < 0.3)
+				map += 'M';
+			else
+				map += 'O';
+		}
+	}
+	return map;
 }
 
-void Load_Map(int seed, int sensitivity,player Pl) {
+void Load_Map(string map, int debelina, player Pl) {
 	cout << (char)201;
 	for (int i = 0; i < 64; i++) {
 		cout << (char)205;
 	}
-	cout << (char)187 << endl; 
-	int Velikost_Ekrana = 64;
-	int zacetna_verednost = Pl.x -32;
-	int visina = Pl.y + 16;
-
-	int j = 0;
-	double n;
-	for (int i = Pl.y - 16; i < visina; i++) {
+	cout << (char)187 << endl;
+	int x = Pl.x-32;
+	int y = Pl.y-32;
+	if (x < 0)
+		x = 0;
+	if (y < 0)
+		y = 0;
+	for (; y < Pl.y + 32; y++) {
 		cout << (char)186;
-		for (int j = zacetna_verednost; j < Pl.x + 32; j++) {
-			n = get_map(seed, sensitivity, j, i);
-			if (Pl.x == j && Pl.y == i) {
+		int x = Pl.x - 32;
+		if (x < 0)
+			x = 0;
+		for (; x < Pl.x + 32; x++) {
+			int naslov = x + y * debelina;
+			
+			if (Pl.x + Pl.y * debelina == naslov) {
 				rlutil::setColor(4);
 				cout << 'P';
 			}
-			else if(n < 0.2){
-				rlutil::setColor(1);
-				cout << (char)176;
-			}
-			else if (n < 0.3) {
-				rlutil::setColor(7);
-				cout << 'M';
-			}
-			else{
-				rlutil::setColor(2);
-				cout << 'O';
+			else {
+				if (map[naslov] == 'O')
+					rlutil::setColor(2);
+				else if (map[naslov] == (char)176)
+					rlutil::setColor(1);
+				else if (map[naslov] == 'M')
+					rlutil::setColor(7);
+				cout << map[naslov];
 			}
 		}
 		rlutil::setColor(7);
-		cout << (char)186;
-		cout << endl;
+		cout << (char)186 << endl;
 	}
+
 	cout << (char)200;
 	for (int i = 0; i < 64; i++) {
 		cout << (char)205;
